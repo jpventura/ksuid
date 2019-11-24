8004ab68ad776359766343605569efd2eacc2e84
JP Ventura <contact@jpventura.com>
2019-11-24 11:52:20 -0300

    muitas coisas sobre documntacao

--- ksuid.py
+++ ksuid.py
@@ -6,8 +6,10 @@ from string import hexdigits
 from typing import NamedTuple
 
 import json
+import string
 import uuid
 
+BASE52 = string.ascii_letters
 BASE_62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 BASE_OF = 62
 
@@ -21,31 +23,119 @@ TABLE_DECODE = dict(zip(
   range(BASE_OF)
 ))
 
+# COMO GARANTIR QUE OS IDs sao praticamente unicos
+# https://towardsdatascience.com/are-uuids-really-unique-57eb80fc2a87
+
+# https://en.wikipedia.org/wiki/Base58
+
+# https://firebase.googleblog.com/2015/02/the-2120-ways-to-ensure-unique_68.html
+# https://gist.github.com/mikelehen/3596a30bd69384624c11
+# A push ID contains 120 bits of information.
+#   The first 48 bits are a timestamp
+#    The timestamp is followed by 72 bits of randomness
+
+# https://en.wikipedia.org/wiki/Base64
+#     ler sobre padding
+
+scheme = {
+  'base58': {
+    'bitcoin': '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+    'tinyurl': '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+  },
+  'base62': {
+    'segmetio': '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  },
+  'base64': {
+    'firebase' : '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz',
+    'firestore': '',
+    'unix'     : 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+  }
+}
+
+scheme2 = {
+  'base58': {
+    'alphabets': {
+      'bitcoin': '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+      'tinyurl': '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+    },
+    'encode': {},
+    'decode': {},
+  },
+  'base62': {
+    'alphabets': {
+      'firebase': '',
+      'segmetio': '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+    },
+    'encode': {},
+    'decode': {},
+  },
+  'base64': {
+    'alphabets': {},
+    'encode': {},
+    'decode': {},
+  }
+}
+
 KSUID_EPOCH_ISO8601 = '2014-05-13T16:53:20.000Z'
 KSUID_EPOCH_TIMESTAMP = 14e8
 KSUID_PAYLOAD = 0x00000000ffffffffffffffffffffffffffffffff
 
+# https://en.wikipedia.org/wiki/Base58
+#    exclude O,0, l, +, / to avoid human error
+# https://stackoverflow.com/questions/695438/safe-characters-for-friendly-urls
+# https://www.ietf.org/rfc/rfc3986.txt
+
+# ASCII sorted
+BASE_58_BITCOIN = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+# NOT ASCII sorted
+BASE_58_SHORT_URL = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+
+ascii_unreserved = ''.join([
+  '+',
+  '-',
+  string.digits,
+  '=',
+  string.ascii_uppercase,
+  '_',
+  string.ascii_lowercase,
+  '~'
+])
 
 
-
-
-
-
-
+# https://en.wikipedia.org/wiki/Base64
+#  '=' padding is used to force 64-bits encoding matching
+ascii_base64 = ''.join([
+  string.digits,
+  string.ascii_uppercase,
+  string.ascii_lowercase,
+  '+', # posso substituir por URL friendly
+  '/', # PRECISO substituir por URL friendly
+])
 
 ID = '0ujzPyRiIAffKhBux4PvQdDqMHY'
 RAW = 0x066A029C73FC1AA3B2446246D6E89FCD909E8FE8
 
-BITS_PER_BYTE =  8
-BYTES_PAYLOAD = 16
-BYTES_KSUID = 20
-BYTES_TIMESTAMP =  4
+BITS_PER_BYTE   =  8     #   1-byte  or   8-bits
+BYTES_PAYLOAD   = 16     #  16-bytes or 128-bits
+BYTES_KSUID     = 20     #  20-bytes or 160-bits
+BYTES_TIMESTAMP =  4     #   4-bytes or  32-bits
 
 KSUID_MIN = 0x0
 KSUID_MAX = 2**160 - 1
 MAX_KSUID_ENCODED = 'aWgEPTl1tmebfsQzFP4bxwgy80V'
 
 MAX_KSUID_ENCODED_LENGTH = len(MAX_KSUID_ENCODED)
+
+# Returns a new ObjectId value. The 12-byte ObjectId value consists of:
+#  a 4-byte timestamp (exact same timestamp size)
+#  a 5-byte random value (ksuid fits here)
+#  a 3-byte counter, starting with a random value.
+
+# chocking https://injuryfacts.nsc.org/all-injuries/preventable-death-overview/odds-of-dying/
+# 7 billion at the same time creating 30-MOLs at the millisecond
+#   5 milisecond abelha, beija-flor
+# 400 milisecond abelha
 
 def _ksuid_check(ksuid):
   if type(ksuid) != str:
@@ -104,15 +194,23 @@ def timestamp(id):
 
 class KSUID(NamedTuple):
   """K-Sorted ID"""
-  created_at : str = KSUID_EPOCH_ISO8601
+  @property
+  def id(self):
+    return "asdf"
   payload    : int = 0
   timestamp  : int = 0
 
   def __str__(self):
     return json.dumps({
-      '_id': str(uuid.UUID(self.payload)),
-      'created_at': self.created_at
-    }, indent=4, sort_keys=True)
+      '_id': self.id,
+      'timestamp': self._isotime(self.timestamp),
+      'payload': str(uuid.UUID(self.payload)),
+    }, sort_keys=True)
+
+  @staticmethod
+  def _isotime(timestamp):
+    moment = datetime.fromtimestamp(timestamp)
+    return '%sZ' % moment.isoformat(timespec='milliseconds')
 
 def inspect(id):
   if (type(id) == str) and set(hexdigits).issuperset(id):
@@ -122,14 +220,16 @@ def inspect(id):
     return inspect(decode(id))
 
   if (type(id) == int) and (KSUID_MIN <= id <= KSUID_MAX):
-    return KSUID(isotime(id), payload(id), timestamp(id))
+    return KSUID(payload(id), timestamp(id))
 
   msg = 'expected id of type int or str, not %s' % type(id).__name__
   raise TypeError(msg)
 
 def main():
-  # print(inspect(ID))
-  print(payload(ID))
+  print(inspect('0oqjvob6nFFOVLKFnJj8Ec6kVnx'))
+  print(timestamp('0oqjvob6nFFOVLKFnJj8Ec6kVnx'))
+  print(format(timestamp(ID), 'x'))
+  
 
 if __name__ == '__main__':
   main()
