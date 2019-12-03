a9d7d19d9a22faed5f188862609d87fd90c4e2dd
JP Ventura <contact@jpventura.com>
2019-12-03 06:59:19 -0300

    Base 62 conversion
    References:
    
      - Base 62 converter
        https://stackoverflow.com/questions/1119722

--- ksuid.py
+++ ksuid.py
@@ -271,3 +271,49 @@ def main():
 
 if __name__ == '__main__':
   main()
+
+
+ID = '0ujzPyRiIAffKhBux4PvQdDqMHY'
+RAW = 0x066A029C73FC1AA3B2446246D6E89FCD909E8FE8
+
+BASE62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encode(num, alphabet=BASE62):
+    """Encode a positive number in Base X
+
+    Arguments:
+    - `num`: The number to encode
+    - `alphabet`: The alphabet to use for encoding
+    """
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+def decode(string, alphabet=BASE62):
+    """Decode a Base X encoded string into the number
+
+    Arguments:
+    - `string`: The encoded string
+    - `alphabet`: The alphabet to use for encoding
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+
+    return num
+
+for i in range(1000):
+  decode(ID)
